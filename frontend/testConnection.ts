9130d9e0828d92e52ec3866995328d9490c4ddb1
mahajanashok2456 <ashokroshan78@gmail.com>
2025-10-12 22:21:45 +0530

    Clean project - removed test files and junk files

--- frontend/testConnection.ts
+++
@@ -1,9 +0,0 @@
-// Simple test to check connection to backend
-fetch('http://localhost:5000/')
-  .then(response => response.text())
-  .then(data => {
-    console.log('Success:', data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
